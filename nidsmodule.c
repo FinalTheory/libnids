c4a6fbbe5bc12d7ad7f09186a977cce40bb58bb7
FinalTheory <FinalTheory@hotmail.com>
2015-09-23 16:28:37 +0800

    modify CMake

--- nidsmodule.c
+++ nidsmodule.c
@@ -20,7 +20,7 @@
 	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111, USA
  */
 
-#include "Python/Python.h"
+#include "Python.h"
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/in_systm.h>
